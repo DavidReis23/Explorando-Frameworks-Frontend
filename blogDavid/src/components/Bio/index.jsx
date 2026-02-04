ede728e7e16a1deeab029228c121e6b5a1636c2c
David Reis <davidsilvareis23@gmail.com>
2026-02-03 21:54:15 -0300

    Atualização da bio

--- blogDavid/src/components/Bio/index.jsx
+++ blogDavid/src/components/Bio/index.jsx
@@ -6,12 +6,9 @@ const Bio = () => {
     <section className="bio">
       <h1>David Reis</h1>
       <p>
-        Coloque aqui sua biografia de forma resumida. Coloque aqui sua biografia
-        de forma resumida. Coloque aqui sua biografia de forma resumida. Coloque
-        aqui sua biografia de forma resumida. Coloque aqui sua biografia de
-        forma resumida. Coloque aqui sua biografia de forma resumida.
-        Desenvolvedor front-end apaixonado por React e tecnologias web.
-        Estudante de desenvolvimento web no quarto semestre.
+        Olá, me chamo David Reis, e sou aluno e Análise e Desenvolvimento de
+        Sistemas no IFCE. Desenvolvedor front-end apaixonado por React e
+        tecnologias web. Estudante de desenvolvimento web no quarto semestre.
       </p>
     </section>
   );
